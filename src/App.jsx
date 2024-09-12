d489c1b149ae22f331f1ebbee0e631301677d24a
pivnati <natanmuleta911@gmail.com>
2024-09-12 08:48:49 -0700

    modfiy the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,16 @@ import Contact from './components/Contact';
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="min-h-screen flex flex-col">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
