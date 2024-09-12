dacaa445cb8cf7a6de768af6fd25cfe949da935c
pivnati <natanmuleta911@gmail.com>
2024-09-12 08:40:57 -0700

    modfiy the header file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
-    return (
-      <header className="bg-black text-white p-4">
-        <div className="max-w-7xl mx-auto flex justify-between items-center">
-          <h1 className="text-3xl font-bold">Nike Shoes</h1>
-          <nav>
-            <ul className="flex space-x-4">
-              <li><a href="#" className="hover:text-gray-300">Home</a></li>
-              <li><a href="#" className="hover:text-gray-300">Shop</a></li>
-              <li><a href="#" className="hover:text-gray-300">About</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
-  }
-  
-  export default Header;
-  
+  return (
+    <header className="bg-black text-white p-4">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <h1 className="text-3xl font-bold">Nike Shoes</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
+            <li><Link to="/shop" className="hover:text-gray-300">Shop</Link></li>
+            <li><Link to="/about" className="hover:text-gray-300">About</Link></li>
+            <li><Link to="/contact" className="hover:text-gray-300">Contact</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
