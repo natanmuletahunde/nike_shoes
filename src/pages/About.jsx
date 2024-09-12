b349d6577b5019b3aecf11adc7b8d90d5de28b32
pivnati <natanmuleta911@gmail.com>
2024-09-12 08:37:53 -0700

    modify the about pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+    return (
+      <div className="bg-gray-100 min-h-screen p-8">
+        <h2 className="text-4xl font-bold mb-4 text-center">About Us</h2>
+        <p className="text-lg text-center">
+          Welcome to Nike! We are committed to delivering top-notch sports gear and fashion-forward footwear.
+        </p>
+      </div>
+    );
+  };
+  
+  export default About;
+  
