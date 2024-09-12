781e09b5c31cf31bd96b25e8a118efbc11f720b6
pivnati <natanmuleta911@gmail.com>
2024-09-12 13:47:27 -0700

    modify the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,25 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Shop from './pages/Shop';
 import Contact from './components/Contact';
-import Cart from './pages/Cart'; // Import Cart page
-import { CartProvider } from './context/CartContext'; // Import CartProvider
+import Cart from './pages/Cart';  // Import Cart page
 
 function App() {
   return (
-    <CartProvider> {/* Wrap the entire app with CartProvider */}
-      <Router>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/cart" element={<Cart />} /> {/* Add route for Cart page */}
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </CartProvider>
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />  {/* Add Cart route */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
